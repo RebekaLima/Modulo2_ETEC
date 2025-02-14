5638c3c36e786583307a1fabd71bf21c6191f142
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-02-13 23:34:25 -0300

    Exercicio de lógica

---
+++ Programação de Aplicativos Mobile I/aula13-02/exercicio1.js
@@ -0,0 +1,12 @@
+let x = 1
+let y = 5
+
+console.log("O valor inicial de x é: " + x)
+console.log("O valor inicial de y é: " + y)
+
+    x = x + y
+    y = x - y
+    x = x - y
+
+console.log("O valor de x é: " + x)
+console.log("O valor de y é: " + y)
