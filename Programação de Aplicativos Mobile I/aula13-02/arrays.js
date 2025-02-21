edf0bb9e2c50195550d1d72dfbbb04b36123b2c9
Rebeka <rebeka.silva.emailprofissional@gmail.com>
2025-02-20 23:49:42 -0300

    arrays corigido

--- Programação de Aplicativos Mobile I/aula13-02/arrays.js
+++ Programação de Aplicativos Mobile I/aula13-02/arrays.js
@@ -2,11 +2,11 @@ let matriz = []
 
 for(linha = 0; linha <10; linha ++){
     matriz[linha] = []
+    
     for (let coluna = 0; coluna < 10; coluna ++) {
         matriz [linha][coluna] = (linha + 1) * (coluna + 2)
-        if(matriz[linha][coluna]%2 >= 1 ){
 
-        }else {
+        if(matriz[linha][coluna]%2 == 0 ){
             console.log(matriz[linha][coluna])
         }
     }
